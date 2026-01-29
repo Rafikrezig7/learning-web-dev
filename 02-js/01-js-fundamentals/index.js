038fcab758a6cbed15e537de4d6a7aee33662248
Rafikrezig7 <Rafikrezig7@users.noreply.github.com>
2026-01-29 23:15:10 +0100

    28/01/26 : reched vid 8 of learning DOM

---
+++ 02-js/01-js-fundamentals/index.js
@@ -0,0 +1,35 @@
+/*  -concatination-
+let message = "Welcome to programming, ";
+message += "Asad!";
+console.log(message);   
+
+    -immutability-
+means that a created word wont be able to chanche it characters we can only edit all the word
+
+    -Temary operations-
+let age=16;
+let name= age>10 ? "pedro" : "carlos";
+
+let name = age>10 && "pedro";
+let name = age>10 || "pedro";
+
+    -Arrow function-
+const/let/var nameFunc =()=>{}
+
+
+    -Array-
+let names=["pedro","sam","jesicca"]
+names.map((name)=>{
+    console.log(name )
+    })
+
+let names=["pedro","sam","jesicca"]
+names.map((name)=>{
+    // console.log(name )
+    return "joe";
+    })
+ 
+let names =["pedro","sam","alex","pedro","pedro"]
+names.filter((name)=>{
+    return name !== "pedro"
+})*/
